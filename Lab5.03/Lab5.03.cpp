822c56f6dd1af9a17254f78f15454b9ffd445fb5
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-24 00:59:22 +0200

    gg

---
+++ Lab5.03/Lab5.03.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double p(const double x);
+int main()
+{
+	double gp, gk, z;
+	int n;
+	cout << "gp = "; cin >> gp;
+	cout << "gk = "; cin >> gk;
+	cout << "n = "; cin >> n;
+	double dg = (gk - gp) / n;
+	double g = gp;
+	while (g <= gk)
+	{
+		z = p(1 - 2 * g)+pow(1-p(1)+pow((2*g),2),2);
+		cout << g << " " << z << endl;
+		g += dg;
+	}
+	return 0;
+}
+double p(const double x)
+{
+	if (abs(x) >= 1)
+		return pow(cos(x),2)-sin(x) / (exp(sin(x)));
+	else
+	{
+		double S = 0;
+		int j = 0;
+		double a = 1;
+		S = 1/cos(x);
+		do
+		{
+			j++;
+			double R =  x / 2;
+			a *= R;
+			S += a;
+		} while (j < 4);
+		return S;
+	}
+}
---
+++ lab5.3 test/lab5.3 test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.03/Lab5.03.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab53test
+{
+	TEST_CLASS(lab53test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			p(1);
+			t = p(-3);
+			Assert::AreEqual(t, 1.14259, 0.01);
+		}
+	};
+}
